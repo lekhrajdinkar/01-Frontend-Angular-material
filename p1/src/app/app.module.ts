f7a21328e7cf983324fc18aabe9a4c6c44ceae0d
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2018-11-11 18:40:30 -0800

    Create Dummy component

--- p1/src/app/app.module.ts
+++ p1/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { TrainingComponent } from './training/training.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    TrainingComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
