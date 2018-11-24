a9ee79911eac632681dfe57d97dc5b6bd1b01a43
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2018-11-24 01:35:03 -0800

    close sideNav once specific item is selected

--- p1/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ p1/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav-list.component.css']
 })
 export class SidenavListComponent implements OnInit {
+  @Output() closeSidenav = new EventEmitter<void>();
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onClose() {
+    this.closeSidenav.emit();
   }
+  
 
 }
